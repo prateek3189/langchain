a6657680e45b5fe6359c145755a00d2668349446
prateek3189 <prateek.magarde@gmail.com>
2025-07-23 19:54:54 +0530

    Add files via upload

---
+++ chatbot/locallama.py
@@ -0,0 +1,39 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import OllamaLLM
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+## Langsmith Tracking (optional)
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+
+## Prompt Template
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant Please respond to the user's queries."),
+    ("user", "Question: {question}")
+])
+
+## streamlit framework
+st.title("Langchain  Demo With OPEN AI")
+input_text = st.text_input("Search the topic you want")
+
+## openAI LLM
+llm = OllamaLLM(model="llama2", temperature=0)
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    response = chain.invoke({"question": input_text})
+    st.write(response)
+
+
